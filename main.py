ecc8877476d405ae3fc6a1798f3bb678c41a8f91
megax <mtellom@correo.uss.cl>
2025-08-21 21:45:24 -0400

    changed port

--- main.py
+++ main.py
@@ -62,4 +62,4 @@ async def itch_user(token: str):
 
 if __name__ == "__main__":
     print("BOT STARTED")
-    uvicorn.run(app, host="0.0.0.0", port=8080)
+    uvicorn.run(app, host="0.0.0.0", port=4468)
